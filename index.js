b44cf700ec51de3c4b3733d901db0ef1ff8e1c75
João Galante <101640788+JoaoGalante12@users.noreply.github.com>
2022-08-02 10:55:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import react from "react";
+import {View,Text} from 'react-native'
+
+export default function sobre(){
+
+    return(
+
+        <View>
+            <Text> tela sobre </Text>
+        </View>
+
+    );
+}
